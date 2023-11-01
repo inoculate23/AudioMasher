95c7ce9f22aefab6ad72aafd361891d319615f1f
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-11-01 10:36:15 -0700

    Create config.go

---
+++ templates/config.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/hex"
+
+	"github.com/JeremyLoy/config"
+	"github.com/gorilla/sessions"
+)
+
+type ConfigData struct {
+	CookieSecret string `config:"MASHER_COOKIE_SECRET"`
+	DBPath       string `config:"MASHER_DB_PATH"`
+	StaticRoot   string `config:"MASHER_STATIC_ROOT"`
+	Port         int    `config:"MASHER_PORT"`
+}
+
+var MasherConfig ConfigData
+
+func LoadConfig(file string) {
+	err := config.From("config/masher.config").FromEnv().To(&MasherConfig)
+	if err != nil {
+		panic(err)
+	}
+}
+
+var SessionStore *sessions.CookieStore
+
+func InitSession() {
+	secret, err := hex.DecodeString(MasherConfig.CookieSecret)
+	if err != nil {
+		panic(err)
+	}
+	SessionStore = sessions.NewCookieStore(secret)
+}
